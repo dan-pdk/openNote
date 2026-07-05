177b4b4b38c23608c2cb5f20e7f351347ad2b946
Arthur-IFPR <arthurvalentinscoparoifpr@gmail.com>
2026-07-05 17:39:27 -0300

    feat: Add basic responsiveness to login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,29 +5,43 @@ import { ReactElement, ReactNode } from "react"
 
 export default function Page() {
     return <>
-        <main className="flex items-center gap-5 flex-col h-screen">
+        <main className="flex items-center gap-5 flex-col h-screen lg:flex-row">
             <Logo />
             <LoginBackground>
-                <h1 className="text-black text-2xl text-center mt-14 dark:text-white">Fazer Login</h1>
-                <div className="bg-white dark:bg-[#1D1D1D] dark:border-white/25 dark:bg- border-2 w-84 h-114 border-black/25 rounded-4xl flex flex-col items-center">
-                    <div className="w-full h-45 flex flex-col items-center justify-center gap-5">
-                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] placeholder:text-black/50 dark:placeholder:text-white/65 pl-5" placeholder="E-mail"></input>
-                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] dark:placeholder:text-white/65 placeholder:text-black/50 pl-5" placeholder="Senha"></input>
+
+                <h1 className="text-black text-2xl text-center max-lg:mt-14 dark:text-white lg:text-5xl">Fazer Login</h1>
+
+                <div className="bg-white dark:bg-[#1D1D1D] dark:border-white/25 dark:bg- border-2 w-84 h-114 border-black/25 rounded-4xl flex flex-col items-center lg:w-150 lg:h-180">
+
+                    <div className="w-full h-45 flex flex-col items-center justify-center gap-5 lg:gap-7.5 lg:mt-15 lg:mb-4">
+                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] placeholder:text-black/50 dark:placeholder:text-white/65 pl-5 lg:w-120 lg:h-15 lg:text-xl " placeholder="E-mail"></input>
+                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] dark:placeholder:text-white/65 placeholder:text-black/50 pl-5 lg:w-120 lg:h-15 lg:text-xl" placeholder="Senha"></input>
                     </div>
-                    <Link href={"#"} className="text-center text-blue-400 underline">Esqueci minha senha</Link>
-                    <input type="button" className="bg-blue-500 w-50 mt-5 p-3 text-center rounded-xl hover:scale-105 duration-200 cursor-pointer" value={"Continuar"}>
+
+                    <Link href={"#"} className="text-center text-blue-400 underline lg:text-xl">Esqueci minha senha</Link>
+
+                    <input type="button" className="bg-blue-500 w-50 mt-5 p-3 text-center rounded-xl hover:scale-105 duration-200 cursor-pointer lg:text-xl lg:mb-5 lg:mt-6.5" value={"Continuar"}>
                     </input>
-                    <div className="w-3/4 h-0.5 bg-black/15 dark:bg-white/15 mt-5"></div>
-                    <p className="text-black font-thin mt-3 dark:text-white">ou</p>
-                    <div className="bg-white flex items-center gap-5 border p-1.5 px-3 border-black/15 rounded-4xl mt-3">
+
+                    <div className="w-3/4 h-0.5 bg-black/15 dark:bg-white/15 mt-5 "></div>
+
+                    <p className="text-black font-thin mt-3 dark:text-white lg:text-xl lg:mt-7">ou</p>
+
+                    <div className="bg-white flex items-center gap-5 border p-1.5 px-3 border-black/15 rounded-4xl mt-3 lg:p-3 lg:px-6 lg:mt-7">
+
                         <Image src={"/google.svg"} width={30} height={30} alt="Google"></Image>
-                        <p className="text-black">Continuar com Google</p>
+                        <p className="text-black lg:text-xl">Continuar com Google</p>
+
                     </div>
-                    <div className="flex text-sm gap-3 mt-5">
+                    <div className="flex text-sm gap-3 mt-5 lg:text-lg lg:mt-10 lg">
+
                         <p className="text-black dark:text-white">Não tem uma conta?</p>
                         <Link href={"#"} className="text-blue-400 underline"> Crie uma Conta</Link>
+
                     </div>
+
                 </div>
+
             </LoginBackground>
 
         </main>
@@ -37,14 +51,21 @@ export default function Page() {
 
 function Logo() {
     return (
-        <Image src={"/LogoMobile.svg"} width={340} height={100} alt=""></Image>
+        <div className="w-2/5 flex justify-center lg:p-10">
+            <div className="lg:hidden">
+                <Image src={"/LogoMobile.svg"} width={340} height={100} alt=""></Image>
+            </div>
+            <div className="max-lg:hidden">
+                <Image src={"/LogoDesktop.svg"} width={450} height={100} alt=""></Image>
+            </div>
+        </div>
     )
 }
 
 function LoginBackground({ children }: { children: ReactNode }) {
     return (
         <>
-            <div className="bg-white size-full flex flex-col items-center gap-10 dark:bg-[#1D1D1D]">
+            <div className="bg-white size-full flex flex-col items-center gap-10 dark:bg-[#1D1D1D] lg:justify-center">
                 {children}
             </div>
         </>
