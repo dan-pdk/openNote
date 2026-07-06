3a6ed3b2fcddbc2bc6edbd59ddf129cbb45fee6c
Arthur-IFPR <arthurvalentinscoparoifpr@gmail.com>
2026-07-06 16:16:05 -0300

    fix: Attempt to fix overscaled GUI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,31 +9,31 @@ export default function Page() {
             <Logo />
             <LoginBackground>
 
-                <h1 className="text-black text-2xl text-center max-lg:mt-14 dark:text-white lg:text-5xl">Fazer Login</h1>
+                <h1 className="text-black text-2xl text-center max-lg:mt-14 dark:text-white lg:text-3xl">Fazer Login</h1>
 
-                <div className="bg-white dark:bg-[#1D1D1D] dark:border-white/25 dark:bg- border-2 w-84 h-114 border-black/25 rounded-4xl flex flex-col items-center lg:w-150 lg:h-180">
+                <div className="bg-white dark:bg-[#1D1D1D] dark:border-white/25 dark:bg- border-2 w-84 h-114 border-black/25 rounded-4xl flex flex-col items-center lg:w-115 lg:h-130">
 
-                    <div className="w-full h-45 flex flex-col items-center justify-center gap-5 lg:gap-7.5 lg:mt-15 lg:mb-4">
-                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] placeholder:text-black/50 dark:placeholder:text-white/65 pl-5 lg:w-120 lg:h-15 lg:text-xl " placeholder="E-mail"></input>
-                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] dark:placeholder:text-white/65 placeholder:text-black/50 pl-5 lg:w-120 lg:h-15 lg:text-xl" placeholder="Senha"></input>
+                    <div className="w-full h-45 flex flex-col items-center justify-center gap-5">
+                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] placeholder:text-black/50 dark:placeholder:text-white/65 pl-5 lg:w-90  " placeholder="E-mail"></input>
+                        <input className="bg-[#E2E2E2] w-64 h-12 rounded-lg dark:bg-[#303030] dark:placeholder:text-white/65 placeholder:text-black/50 pl-5 lg:w-90 " placeholder="Senha"></input>
                     </div>
 
-                    <Link href={"#"} className="text-center text-blue-400 underline lg:text-xl">Esqueci minha senha</Link>
+                    <Link href={"#"} className="text-center text-blue-400 underline ">Esqueci minha senha</Link>
 
-                    <input type="button" className="bg-blue-500 w-50 mt-5 p-3 text-center rounded-xl hover:scale-105 duration-200 cursor-pointer lg:text-xl lg:mb-5 lg:mt-6.5" value={"Continuar"}>
+                    <input type="button" className="bg-blue-500 w-50 mt-5 p-3 text-center rounded-xl hover:scale-105 duration-200 cursor-pointer  lg:mt-6.5" value={"Continuar"}>
                     </input>
 
                     <div className="w-3/4 h-0.5 bg-black/15 dark:bg-white/15 mt-5 "></div>
 
-                    <p className="text-black font-thin mt-3 dark:text-white lg:text-xl lg:mt-7">ou</p>
+                    <p className="text-black font-thin mt-3 dark:text-white ">ou</p>
 
-                    <div className="bg-white flex items-center gap-5 border p-1.5 px-3 border-black/15 rounded-4xl mt-3 lg:p-3 lg:px-6 lg:mt-7">
+                    <div className="bg-white flex items-center gap-5 border p-1.5 px-3 border-black/15 rounded-4xl mt-3 lg:p-3 lg:px-6 ">
 
                         <Image src={"/google.svg"} width={30} height={30} alt="Google"></Image>
-                        <p className="text-black lg:text-xl">Continuar com Google</p>
+                        <p className="text-black ">Continuar com Google</p>
 
                     </div>
-                    <div className="flex text-sm gap-3 mt-5 lg:text-lg lg:mt-10 lg">
+                    <div className="flex text-sm gap-3 mt-5 lg:text-base lg:mt-10">
 
                         <p className="text-black dark:text-white">Não tem uma conta?</p>
                         <Link href={"#"} className="text-blue-400 underline"> Crie uma Conta</Link>
